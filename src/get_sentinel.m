cbf3bdb1fc87ce9cf3473d266ffecfb4ca174861
ZIQING YU <ziqing.yu0623@gmail.com>
2023-01-12 13:47:40 +0100

    update

--- src/get_sentinel.m
+++ src/get_sentinel.m
@@ -1,14 +1,19 @@
 clc
 
-S3Afile = "../../S3A/S3A1004_p3101t586o110sr2000wo1or99.mat";
+S3Afile = "C:\Users\yuziq\Documents\gnss_ir_analyze\data\S3AEsslingen_s1_p3101t586o110sr1000or80.mat";
 load(S3Afile)
 
-lat = S3A1004_p3101t586o110sr2000wo1or99.ObjVS.Gen.Sat.Lat.Hi.C.Signal;
-lon = S3A1004_p3101t586o110sr2000wo1or99.ObjVS.Gen.Sat.Lon.Hi.C.Signal;
-time = S3A1004_p3101t586o110sr2000wo1or99.ObjVS.Gen.Sat.Lon.Hi.C.Time;
+lat = S3AEsslingen_s1_p3101t586o110sr1000or80.ObjVS.Gen.Sat.Lat.Hi.C.Signal;
+lon = S3AEsslingen_s1_p3101t586o110sr1000or80.ObjVS.Gen.Sat.Lon.Hi.C.Signal;
+time = S3AEsslingen_s1_p3101t586o110sr1000or80.ObjVS.Gen.Sat.Lon.Hi.C.Time;
 time = datetime(time,'ConvertFrom','datenum');
 
-lat = S3A1004_p3101t586o110sr2000wo1or99.ObjVS.Raw.Sat.Lat.Hi.C.Signal;
-lon = S3A1004_p3101t586o110sr2000wo1or99.ObjVS.Raw.Sat.Lon.Hi.C.Signal;
-time = S3A1004_p3101t586o110sr2000wo1or99.ObjVS.Raw.Sat.Lon.Hi.C.Time;
+lat = S3AEsslingen_s1_p3101t586o110sr1000or80.ObjVS.Raw.Sat.Lat.Hi.C.Signal;
+lon = S3AEsslingen_s1_p3101t586o110sr1000or80.ObjVS.Raw.Sat.Lon.Hi.C.Signal;
+time = S3AEsslingen_s1_p3101t586o110sr1000or80.ObjVS.Raw.Sat.Lon.Hi.C.Time;
 time = datetime(time,'ConvertFrom','datenum');
+
+% 导出
+% unix_t = posixtime(datetime(t,'convertfrom','datenum'));
+% result
+% writematrix
