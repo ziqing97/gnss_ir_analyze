3fc4a307a15c3aec0ecaaca8972a3d8943ddd747
ZIQING YU <ziqing.yu0623@gmail.com>
2023-01-14 15:59:12 +0100

    update

--- src/get_sentinel.m
+++ src/get_sentinel.m
@@ -3,6 +3,8 @@ clc
 S3Afile = "C:\Users\yuziq\Documents\gnss_ir_analyze\data\S3AEsslingen_s1_p3101t586o110sr1000or80.mat";
 load(S3Afile)
 
+object = S3AEsslingen_s1_p3101t586o110sr1000or80;
+
 lat = S3AEsslingen_s1_p3101t586o110sr1000or80.ObjVS.Gen.Sat.Lat.Hi.C.Signal;
 lon = S3AEsslingen_s1_p3101t586o110sr1000or80.ObjVS.Gen.Sat.Lon.Hi.C.Signal;
 time = S3AEsslingen_s1_p3101t586o110sr1000or80.ObjVS.Gen.Sat.Lon.Hi.C.Time;
@@ -17,3 +19,7 @@ time = datetime(time,'ConvertFrom','datenum');
 % unix_t = posixtime(datetime(t,'convertfrom','datenum'));
 % result
 % writematrix
+
+sat_height = object.objCropData.Raw.Sata.Alt.Hi.Ku.Signal;
+range = object.objCropData.(object.ObjTS.deepA.range);
+corr = object.objCropData.(object.ObjTS.deepA.InvBar);
