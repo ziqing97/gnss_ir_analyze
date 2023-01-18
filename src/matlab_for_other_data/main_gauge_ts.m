79d8aa44d9d8706ecc719825bcfc81605cb5216c
ziqing97 <ziqing.yu0623@gmail.com>
2023-01-18 19:04:34 +0100

    add for november

--- src/matlab_for_other_data/main_gauge_ts.m
+++ src/matlab_for_other_data/main_gauge_ts.m
@@ -17,3 +17,8 @@ t1 = posixtime(datetime(2022,10,31,8,30,0,'TimeZone','UTC'));
 t2 = posixtime(datetime(2022,10,31,11,30,0,'TimeZone','UTC'));
 name = '20221031_gauge.csv';
 generate_gauge_ts(t1,t2,name)
+
+t1 = posixtime(datetime(2022,11,27,8,30,0,'TimeZone','UTC'));
+t2 = posixtime(datetime(2022,11,27,11,30,0,'TimeZone','UTC'));
+name = '20221127_gauge.csv';
+generate_gauge_ts(t1,t2,name)
