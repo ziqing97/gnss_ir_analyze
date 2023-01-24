d73d198715737647ba65c65ac7d6dbc2f705c359
ziqing97 <ziqing.yu0623@gmail.com>
2023-01-24 23:37:21 +0100

    update matlab script getting things from sentinel

---
+++ src/matlab_for_other_data/get_height_from_altbundle.m
@@ -0,0 +1,62 @@
+%% Init
+clc;close all;clearvars
+
+%% Load Data (height in ellipse)
+S3Afile = "../../data/altbundle/S3AEsslingen_s1_p3101t586o110sr1000or80.mat";
+load(S3Afile)
+data = S3AEsslingen_s1_p3101t586o110sr1000or80;
+
+WL_struct = data.ObjTS.Full.WL;
+retrack = fields(WL_struct);
+retrackname  = {'OCES','OCEP','OGS','ISS','ICES','ICEP','SICS'};
+
+sig_tab = [];
+for i=1:length(retrack)
+    if i==1
+        time = datetime(WL_struct.(retrack{i}).Time,'ConvertFrom','datenum','TimeZone','UTC');
+        sig_tab = [sig_tab,posixtime(time)/1000];
+    end
+    sig_tab = [sig_tab,WL_struct.(retrack{i}).Signal];
+end
+
+% 11.Aug
+time_start = datetime(2022,8,11,8,0,0,'TimeZone','UTC');
+time_end = datetime(2022,8,11,12,0,0,'TimeZone','UTC');
+
+index = (time>time_start) & (time<time_end);
+h_sentinel = sig_tab(index,:);
+figure
+hold on
+for i=1:7
+    scatter(h_sentinel(:,1),h_sentinel(:,i+1))
+end
+title("11.Aug")
+ylabel('meter')
+
+% 07.Sep
+time_start = datetime(2022,9,7,8,0,0,'TimeZone','UTC');
+time_end = datetime(2022,9,7,12,0,0,'TimeZone','UTC');
+
+index = (time>time_start) & (time<time_end);
+h_sentinel = sig_tab(index,:);
+figure
+hold on
+for i=1:7
+    scatter(h_sentinel(:,1),h_sentinel(:,i+1))
+end
+title("07.Sep")
+ylabel('meter')
+
+% 04.Oct
+time_start = datetime(2022,10,4,8,0,0,'TimeZone','UTC');
+time_end = datetime(2022,10,4,12,0,0,'TimeZone','UTC');
+
+index = (time>time_start) & (time<time_end);
+h_sentinel = sig_tab(index,:);
+figure
+hold on
+for i=1:7
+    scatter(h_sentinel(:,1),h_sentinel(:,i+1))
+end
+title("04.Oct")
+ylabel('meter')
