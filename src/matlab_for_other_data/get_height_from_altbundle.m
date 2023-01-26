4fa935d9557bcbaf25e3930aa2eec91ff205c21f
ZIQING YU <ziqing.yu0623@gmail.com>
2023-01-26 11:05:18 +0100

    Update get_height_from_altbundle.m

--- src/matlab_for_other_data/get_height_from_altbundle.m
+++ src/matlab_for_other_data/get_height_from_altbundle.m
@@ -14,12 +14,16 @@ sig_tab = [];
 for i=1:length(retrack)
     if i==1
         time = datetime(WL_struct.(retrack{i}).Time,'ConvertFrom','datenum','TimeZone','UTC');
-        sig_tab = [sig_tab,posixtime(time)/1000];
+        lat = data.ObjVS.Raw.Sat.Lat.Hi.Ku.Signal;
+        lon = data.ObjVS.Raw.Sat.Lon.Hi.Ku.Signal;
+        sig_tab = [sig_tab,posixtime(time),lat,lon];
     end
     sig_tab = [sig_tab,WL_struct.(retrack{i}).Signal];
 end
 
-% 11.Aug
+tab_title = [{'unixtime'},{'lat'},{'lon'},retrackname];
+
+%% 11.Aug
 time_start = datetime(2022,8,11,8,0,0,'TimeZone','UTC');
 time_end = datetime(2022,8,11,12,0,0,'TimeZone','UTC');
 
@@ -28,12 +32,17 @@ h_sentinel = sig_tab(index,:);
 figure
 hold on
 for i=1:7
-    scatter(h_sentinel(:,1),h_sentinel(:,i+1))
+    scatter(datetime(h_sentinel(:,1),'ConvertFrom','epochtime'),h_sentinel(:,i+3))
 end
+legend(retrackname)
 title("11.Aug")
 ylabel('meter')
 
-% 07.Sep
+%
+T = array2table(h_sentinel, "VariableNames",tab_title);
+writetable(T,'../../data/altbundle/20220811sentinel.csv')
+
+%% 07.Sep
 time_start = datetime(2022,9,7,8,0,0,'TimeZone','UTC');
 time_end = datetime(2022,9,7,12,0,0,'TimeZone','UTC');
 
@@ -42,12 +51,17 @@ h_sentinel = sig_tab(index,:);
 figure
 hold on
 for i=1:7
-    scatter(h_sentinel(:,1),h_sentinel(:,i+1))
+    scatter(datetime(h_sentinel(:,1),'ConvertFrom','epochtime'),h_sentinel(:,i+3))
 end
+legend(retrackname)
 title("07.Sep")
 ylabel('meter')
 
-% 04.Oct
+%
+T = array2table(h_sentinel, "VariableNames",tab_title);
+writetable(T,'../../data/altbundle/20220907sentinel.csv')
+
+%% 04.Oct
 time_start = datetime(2022,10,4,8,0,0,'TimeZone','UTC');
 time_end = datetime(2022,10,4,12,0,0,'TimeZone','UTC');
 
@@ -56,7 +70,12 @@ h_sentinel = sig_tab(index,:);
 figure
 hold on
 for i=1:7
-    scatter(h_sentinel(:,1),h_sentinel(:,i+1))
+    scatter(datetime(h_sentinel(:,1),'ConvertFrom','epochtime'),h_sentinel(:,i+3))
 end
+legend(retrackname)
 title("04.Oct")
 ylabel('meter')
+
+%
+T = array2table(h_sentinel, "VariableNames",tab_title);
+writetable(T,'../../data/altbundle/20221004sentinel.csv')
