578c9d1620e9d7d3f13b86f83140f07cae2eea47
ziqing97 <ziqing.yu0623@gmail.com>
2023-01-28 10:51:54 +0100

    Update get_height_from_altbundle.m

--- src/matlab_for_other_data/get_height_from_altbundle.m
+++ src/matlab_for_other_data/get_height_from_altbundle.m
@@ -2,9 +2,9 @@
 clc;close all;clearvars
 
 %% Load Data (height in ellipse)
-S3Afile = "../../data/altbundle/S3AEsslingen_s1_p3101t586o110sr1000or80.mat";
+S3Afile = "../../data/altbundle/S3AEsslingen_s1_p3101t586o110sr800or80.mat";
 load(S3Afile)
-data = S3AEsslingen_s1_p3101t586o110sr1000or80;
+data = S3AEsslingen_s1_p3101t586o110sr800or80;
 
 WL_struct = data.ObjTS.Full.WL;
 retrack = fields(WL_struct);
@@ -14,16 +14,12 @@ sig_tab = [];
 for i=1:length(retrack)
     if i==1
         time = datetime(WL_struct.(retrack{i}).Time,'ConvertFrom','datenum','TimeZone','UTC');
-        lat = data.ObjVS.Raw.Sat.Lat.Hi.Ku.Signal;
-        lon = data.ObjVS.Raw.Sat.Lon.Hi.Ku.Signal;
-        sig_tab = [sig_tab,posixtime(time),lat,lon];
+        sig_tab = [sig_tab,posixtime(time)/1000];
     end
     sig_tab = [sig_tab,WL_struct.(retrack{i}).Signal];
 end
 
-tab_title = [{'unixtime'},{'lat'},{'lon'},retrackname];
-
-%% 11.Aug
+% 11.Aug
 time_start = datetime(2022,8,11,8,0,0,'TimeZone','UTC');
 time_end = datetime(2022,8,11,12,0,0,'TimeZone','UTC');
 
@@ -32,17 +28,12 @@ h_sentinel = sig_tab(index,:);
 figure
 hold on
 for i=1:7
-    scatter(datetime(h_sentinel(:,1),'ConvertFrom','epochtime'),h_sentinel(:,i+3))
+    scatter(h_sentinel(:,1),h_sentinel(:,i+1))
 end
-legend(retrackname)
 title("11.Aug")
 ylabel('meter')
 
-%
-T = array2table(h_sentinel, "VariableNames",tab_title);
-writetable(T,'../../data/altbundle/20220811sentinel.csv')
-
-%% 07.Sep
+% 07.Sep
 time_start = datetime(2022,9,7,8,0,0,'TimeZone','UTC');
 time_end = datetime(2022,9,7,12,0,0,'TimeZone','UTC');
 
@@ -51,17 +42,12 @@ h_sentinel = sig_tab(index,:);
 figure
 hold on
 for i=1:7
-    scatter(datetime(h_sentinel(:,1),'ConvertFrom','epochtime'),h_sentinel(:,i+3))
+    scatter(h_sentinel(:,1),h_sentinel(:,i+1))
 end
-legend(retrackname)
 title("07.Sep")
 ylabel('meter')
 
-%
-T = array2table(h_sentinel, "VariableNames",tab_title);
-writetable(T,'../../data/altbundle/20220907sentinel.csv')
-
-%% 04.Oct
+% 04.Oct
 time_start = datetime(2022,10,4,8,0,0,'TimeZone','UTC');
 time_end = datetime(2022,10,4,12,0,0,'TimeZone','UTC');
 
@@ -70,12 +56,35 @@ h_sentinel = sig_tab(index,:);
 figure
 hold on
 for i=1:7
-    scatter(datetime(h_sentinel(:,1),'ConvertFrom','epochtime'),h_sentinel(:,i+3))
+    scatter(h_sentinel(:,1),h_sentinel(:,i+1))
 end
-legend(retrackname)
 title("04.Oct")
 ylabel('meter')
 
-%
-T = array2table(h_sentinel, "VariableNames",tab_title);
-writetable(T,'../../data/altbundle/20221004sentinel.csv')
+% 31.Oct
+time_start = datetime(2022,10,31,8,0,0,'TimeZone','UTC');
+time_end = datetime(2022,10,31,12,0,0,'TimeZone','UTC');
+
+index = (time>time_start) & (time<time_end);
+h_sentinel = sig_tab(index,:);
+figure
+hold on
+for i=1:7
+    scatter(h_sentinel(:,1),h_sentinel(:,i+1))
+end
+title("31.Oct")
+ylabel('meter')
+
+% 27.Nov
+time_start = datetime(2022,11,27,8,0,0,'TimeZone','UTC');
+time_end = datetime(2022,11,27,12,0,0,'TimeZone','UTC');
+
+index = (time>time_start) & (time<time_end);
+h_sentinel = sig_tab(index,:);
+figure
+hold on
+for i=1:7
+    scatter(h_sentinel(:,1),h_sentinel(:,i+1))
+end
+title("27.Nov")
+ylabel('meter')
