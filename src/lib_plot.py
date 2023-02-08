ea9b147f4528528d4523897b24d3d713610d21de
ZIQING YU <ziqing.yu0623@gmail.com>
2023-02-08 16:50:37 +0100

    Update lib_plot.py

--- src/lib_plot.py
+++ src/lib_plot.py
@@ -4,6 +4,7 @@ This script provides the function for plotting
 Author: Ziqing Yu
 Last edited on 08/02/2023
 '''
+from datetime import datetime,timedelta,timezone
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -23,8 +24,8 @@ def plot_timeseries(ts_antenne1, ts_antenne2, h_antenne_1, h_antenne_2,\
         retrack_method (_type_): _description_
         datekey (_type_): _description_
     """
-    month = datekey[0:2]
-    date = datekey[2:]
+    month = int(datekey[0:2])
+    date = int(datekey[2:])
     h_ts_0811_2 = h_antenne_1 - np.asarray(list(ts_antenne1.values()))
     h_ts_0811_3 = h_antenne_2 - np.asarray(list(ts_antenne2.values()))
     # plot all
@@ -37,7 +38,10 @@ def plot_timeseries(ts_antenne1, ts_antenne2, h_antenne_1, h_antenne_2,\
     ax.set_xlabel('time (date hour:minute)')
     ax.set_ylabel('heihgt(meter)')
     ax.set_title(f'water surface height {date}.{month} (elevation 0-30, time window 30 minutes)')
+    ax.set_xlim([datetime(year=2022,month=month,day=date,hour=8,\
+            minute=30,second=0,tzinfo=timezone.utc),datetime(year=2022,month=month,day=date,hour=11,\
+            minute=0,second=0,tzinfo=timezone.utc)])
     ax.set_ylim([283,285])
     ax.legend(['gnss antenne 1','gnss antenne 2','gauge',retrack_method])
-    fig.set_size_inches(9,3)
+    fig.set_size_inches(9,2)
     fig.savefig(f'picture/timeseries{datekey}.png')
