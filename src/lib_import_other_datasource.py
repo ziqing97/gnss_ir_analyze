9e9712e2cbfa16cb72dfa8a41714559ab1e92066
ziqingyu <ziqing.yu0623@gmail.com>
2023-05-06 19:24:43 +0200

    add antenna offset

--- src/lib_import_other_datasource.py
+++ src/lib_import_other_datasource.py
@@ -20,14 +20,14 @@ def get_antenne_height(key):
     index = df_ante_h[df_ante_h['date'] == key]
     if key == '0216':
         ante_h['1'] = float(index['antenne1'])
-        ante_h['2'] = float(index['antenne2'])
+        ante_h['2'] = float(index['antenne2'])+0.2
         ante_h['2r'] = float(index['antenne2r'])
     elif key[0:-2] == '0315':
         ante_h['1'] = float(index['antenne1'])
         ante_h['r'] = float(index['antenne2r'])
     else:
-        ante_h['2'] = float(index['antenne2'])
-        ante_h['3'] = float(index['antenne3'])
+        ante_h['2'] = float(index['antenne2'])+0.2
+        ante_h['3'] = float(index['antenne3'])+0.2
     return ante_h
 
 def get_gauge_data(date_key):
